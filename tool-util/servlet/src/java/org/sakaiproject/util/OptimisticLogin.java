656265a55544892051a5a00a3afb91d313301d16
Matthew Buckett <matthew.buckett@oucs.ox.ac.uk>
2009-07-03 12:22:37 +0100

    Quick hack at an optimistic login.

---
+++ tool-util/servlet/src/java/org/sakaiproject/util/OptimisticLogin.java
@@ -0,0 +1,105 @@
+package org.sakaiproject.util;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.sakaiproject.tool.cover.SessionManager;
+
+/**
+ * Filter which looks for a request that could be logged in but isn't and
+ * redirects, preserving the original URL. Must go after the sakai 
+ * request filter.
+ * 
+ * @author buckett
+ * 
+ */
+public class OptimisticLogin implements Filter {
+
+	private static final Log log = LogFactory.getLog(OptimisticLogin.class);
+
+	private String cookieName = "webauth_found";
+
+	private boolean strictDomain = true;
+
+	private String loginUrl = "/sakai-login-tool/container";
+
+	public void destroy() {
+		log.debug("destroy");
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		if (request instanceof HttpServletRequest
+				&& response instanceof HttpServletResponse) {
+			HttpServletRequest httpRequest = (HttpServletRequest) request;
+			if (hasCookie(httpRequest)) {
+				String userId = SessionManager.getCurrentSessionUserId();
+				if (userId == null) {
+					// Ok, time to redirect then.
+					HttpServletResponse httpResponse = (HttpServletResponse) response;
+					StringBuffer originalUrl = httpRequest.getRequestURL();
+					String queryString = httpRequest.getQueryString();
+					if (queryString != null) {
+							originalUrl.append("?").append(httpRequest.getQueryString());
+					}
+					String redirectUrl = new StringBuilder(loginUrl).append(
+							"?redirect=").append(
+							URLEncoder.encode(originalUrl.toString(), "UTF-8")).toString();
+					httpResponse.sendRedirect(redirectUrl);
+					return;
+				}
+			}
+		}
+		chain.doFilter(request, response);
+	}
+
+	public void init(FilterConfig filterConfig) throws ServletException {
+		log.debug("init");
+	}
+
+	/**
+	 * See if the request has the magic cookie.
+	 * 
+	 * @param request
+	 *            ServletRequest
+	 * @return true if it does.
+	 */
+	private boolean hasCookie(HttpServletRequest request) {
+		boolean hasCookie = false;
+		boolean multipleMatches = false;
+		String domain = request.getServerName();
+		if (request.getCookies() != null) {
+			for (Cookie cookie : request.getCookies()) {
+				if (cookieName.equals(cookie.getName())) {
+					// If found multiple cookie.
+					multipleMatches = hasCookie;
+					// IE seems to send too many cookies.
+					if (strictDomain) {
+						if (cookie.getDomain() == null || domain.equals(cookie.getDomain())) {
+							hasCookie = true;
+						}
+					} else {
+						hasCookie = true;
+					}
+				}
+			}
+			if (multipleMatches && log.isDebugEnabled()) {
+				log.debug("Multiple cookies for '" + cookieName + "' found.");
+			}
+		}
+		return hasCookie;
+	}
+
+}
